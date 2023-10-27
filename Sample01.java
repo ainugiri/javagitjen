ac694ccc3593a1ec79b7ed70fefe6d3e17c66cf2
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-10-27 16:52:58 +0530

    Add files via upload

---
+++ Sample01.java
@@ -0,0 +1,17 @@
+class Demo{
+public static void main(String giri[]){
+ int i = 5;
+ for(i=5;i<=10;i++){
+     System.out.println("Class Name Demo");
+}
+}
+}
+
+class Sample01{
+public static void main(String giri[]){
+ int i = 5;
+ for(i=5;i<=10;i++){
+     System.out.println("Class Name Sample01");
+}
+}
+}
